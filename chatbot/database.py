a61f66594933ec3bafe038f40be62d00d15bdab2
OwoFlow User <user@example.com>
2025-12-26 15:49:41 +0100

    Fix database connection code

--- chatbot/database.py
+++ chatbot/database.py
@@ -1,21 +1,33 @@
-"""Database connection for Azure SQL using SQLAlchemy."""
+import os
+import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import urllib.parse
-import os
 
-# Construct connection string
-params = urllib.parse.quote_plus(
-    'Driver={ODBC Driver 18 for SQL Server};'
-    'Server=tcp:kofa-server-bane.database.windows.net,1433;'
-    'Database=Kofa-db;'
-    'Uid=Admin-david;'
-    'Pwd=' + os.getenv("DB_PASSWORD", "") + ';'
-    'Encrypt=yes;'
-    'TrustServerCertificate=no;'
-    'Connection Timeout=30;'
+# 1. READ CREDENTIALS FROM HEROKU "SAFE BOX"
+# (If these are missing, it defaults to the values you provided)
+server = os.getenv("DB_SERVER", "kofa-server-bane.database.windows.net")
+database = os.getenv("DB_NAME", "Kofa-db")
+username = os.getenv("DB_USER", "Admin-david")
+password = os.getenv("DB_PASSWORD", "Chiwenduezi619")
+driver = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
+
+# 2. BUILD THE KEY
+# We MUST use TrustServerCertificate=yes for Heroku to talk to Azure
+connection_string = (
+    f"Driver={{{driver}}};"
+    f"Server=tcp:{server},1433;"
+    f"Database={database};"
+    f"Uid={username};"
+    f"Pwd={password};"
+    "Encrypt=yes;"
+    "TrustServerCertificate=yes;"
+    "Connection Timeout=30;"
 )
+
+# 3. UNLOCK THE DOOR
+params = urllib.parse.quote_plus(connection_string)
 conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+
 engine = create_engine(conn_str)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
